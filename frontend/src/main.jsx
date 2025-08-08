a4c58cec4ba4b3c95a4d5747a58c4404dd4cd03b
Sudarshan <asudarshan14@gmail.com>
2025-08-08 13:57:16 -0400

    gh-pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+)
